81de2f2c46e56eb07c46bc9394851dc08ac4dfc1
EmersonManabuAraki <manabu-araki@hotmail.com>
2019-08-07 11:10:11 -0300

    Refactored category selection logic

--- main.rb
+++ main.rb
@@ -4,35 +4,31 @@ load 'player.rb'
 require 'json'
 
 file = File.read("category.json")
-categories = JSON.parse(file)
-categories_array = []
+categories_array = JSON.parse(file)
+categories = []
 i = 0
 
-categories.each do |key, words| 
-    categories_array[i] = Category.new(words)
+categories_array.each do |name, words| 
+    categories[i] = Category.new(name, words)
     i += 1
 end
 
 
-puts "Select a category: \n 1 - Fruits\n 2 - Geometry\n 3 - Foods\n 4 - Electronics\n"
+puts "Select a category: \n"
+categories.each do |category|
+    puts "- #{category.name}\n"
+end
 
+puts "Write the category name"
 selector = gets.chomp
 
-case selector
-when "1"
-    selector = categories_array[0].category
-    puts "You've selected Fruits"
-when "2"
-    selector = categories_array[1].category
-    puts "You've selected Geometry"
-when "3"
-    selector = categories_array[2].category
-    puts "You've selected Foods"
-when "4"
-    selector = categories_array[3].category
-    puts "You've selected Electronics"
-else
-    puts "You write something wrong, try again!"
+categories.each do |category|
+    case selector
+    when category.name 
+        puts "You've selected #{category.name}"
+        selector = category.words
+        break
+    end
 end
 
 forca_word = Forca.new
@@ -41,29 +37,40 @@ forca_word = forca_word.random_elements(selector)
 the_word_cripted = Forca.cript_word(forca_word.scan(/./))
 
 while(player.chance > 0)
+    puts "\n"
     puts "Chances left: #{player.chance}"
     puts "Words you attempted: #{player.letters}"
     puts "Word: #{the_word_cripted.inspect}"
     try = gets.chomp
     if try == forca_word
         puts "YOU WIN!"
-        puts "SCORE: #{player.get_points_perfect}"
+        puts "SCORE: #{player.get_points(forca_word)}"
         break
     end
     if try.size > 1 && try != forca_word 
         puts "GAME OVER!"
         break
     end
-    if forca_word.include?(try) 
+    if forca_word.include?(try)
+        if the_word_cripted.include?(try)
+            puts "You already input this letter"
+            next
+        end 
         index = forca_word.scan(/./).each_with_index.select {|word, index| word == try}.map {|pair| pair[1]}
         index.each {|e| the_word_cripted[e] = try }
         player.play_count_hit
+    elsif player.letters.include?(try) 
+        puts "You already tried this letter"
+        next
+    elsif try == " "
+        puts "You can't type blank space "
+        next
     else
         player.play_count_miss(try)
     end
     if the_word_cripted == forca_word.scan(/./) 
         puts "YOU WIN!"
-        puts "SCORE: #{player.get_points}"
+        puts "SCORE: #{player.get_points(forca_word)}"
         break
     end
     if player.chance == 0 
